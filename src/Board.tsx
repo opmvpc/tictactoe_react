cbd8b40a0c043d8cb01c25b7c26400c0010908e2
jonathan <wow_Haukins@hotmail.com>
2024-03-25 17:02:30 +0100

    wip

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,12 +2,18 @@ import { MouseEventHandler, useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (index: number) => {
+    if (squares[index] !== null) {
+      return;
+    }
+
     const newSquares = squares.slice();
-    newSquares[index] = "X";
+    newSquares[index] = xIsNext ? "X" : "O";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   };
 
   return (
